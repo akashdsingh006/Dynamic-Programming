a0b32a213e14e8e345f30e61a3f4b111075f8089
Akash Anand <82928605+akashdsingh006@users.noreply.github.com>
2023-03-14 01:56:33 +0530

    Update Memoization.java

--- DP on Strings/Longest common subsequence/Memoization.java
+++ DP on Strings/Longest common subsequence/Memoization.java
@@ -1,17 +1,17 @@
-class Solution
+ Solution
 {
     //Function to find the length of longest common subsequence in two strings.
     static int lcs(int x, int y, String s1, String s2)
     {
         // your code here
-        int dp[][]=new int[s1.length()][s2.length()];
+        int dp[][]=new int[x+1][y+1];
         for(int row[]:dp)
             Arrays.fill(row,-1);
-        return getLCS(s1.length()-1,s2.length()-1,s1,s2,dp);
+        return getLCS(x,y,s1,s2,dp);
     }
     static int getLCS(int idx1,int idx2,String s1,String s2,int[][]dp)
     {
-        if(idx1<0||idx2<0)
+        if(idx1==0||idx2==0)
         {
             return 0;
         }
@@ -19,7 +19,7 @@ class Solution
             return dp[idx1][idx2];
             
             
-        if(s1.charAt(idx1)==s2.charAt(idx2))
+        if(s1.charAt(idx1-1)==s2.charAt(idx2-1))
         {
             return 1+getLCS(idx1-1,idx2-1,s1,s2,dp);
         }
